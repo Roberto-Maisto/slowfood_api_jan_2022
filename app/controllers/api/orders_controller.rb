5b71dba9c4ce4044086928fcecfa323e7ed99735
Thomas Ochman <thomas@dragster.se>
2022-03-02 16:08:57 +0100

    extracts rescue block to application controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -5,9 +5,7 @@ class Api::OrdersController < ApplicationController
     order = current_user.orders.create
     product = Product.find(params[:product_id])
     order.items.create(product: product)
-
     render json: { message: 'Your order was created...' }, status: 201
-  rescue StandardError
-    render json: { error: 'Your request could not be processed...' }, status: 422
   end
+
 end
