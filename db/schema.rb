13d0c36a0e202fb7667ff86cafa23b19d8446d5a
Thomas Ochman <thomas@dragster.se>
2022-03-02 12:15:29 +0100

    creates orders controller with create action adds create order route creates user spec creates order model with belongs_to association to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_104354) do
+ActiveRecord::Schema.define(version: 2022_03_02_110246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -50,4 +57,5 @@ ActiveRecord::Schema.define(version: 2022_03_02_104354) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
