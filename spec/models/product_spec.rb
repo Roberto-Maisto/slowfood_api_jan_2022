d00be97501192a4ae8866504019550dd2072fedb
Thomas Ochman <thomas@dragster.se>
2022-03-02 10:30:34 +0100

    updates product specs to cover existing db columns and factory

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,21 @@
 RSpec.describe Product, type: :model do
   describe 'Database' do
-it { is_expected.to have_db_coloumn(:name).of_type(:string)}
-end
-describe 'Factory' do
-end
-
+    it {
+      is_expected.to have_db_column(:name)
+        .of_type(:string)
+    }
+    it {
+      is_expected.to have_db_column(:price)
+        .of_type(:float)
+    }
+    it {
+      is_expected.to have_db_column(:description)
+        .of_type(:text)
+    }
+  end
+  describe 'Factory' do
+    it 'is expected to be valid' do
+      expect(create(:product)).to be_valid
+    end
+  end
 end
