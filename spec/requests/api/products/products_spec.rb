a7bb80ea9227b4b45409df82c8fde46be1774afd
Thomas Ochman <thomas@dragster.se>
2022-03-02 10:45:50 +0100

    simplifies spec for response status

--- spec/requests/api/products/products_spec.rb
+++ spec/requests/api/products/products_spec.rb
@@ -3,12 +3,10 @@ RSpec.describe 'GET /api/products', type: :request do
   before do
     get '/api/products'
   end
-
+  
   subject { response }
 
-  it 'is expected to respond with status code 200 ' do
-    expect(subject.status).to eq 200
-  end
+  it { is_expected.to have_http_status 200 }
 
   it 'is expected to return a list of products' do
     expect(response_json['products'].count).to eq 3
